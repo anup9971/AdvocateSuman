8e5a1105ec9536814ea6ae7feb77fd64957c8825
devparmanand <pjha3861@gmail.com>
2025-08-08 01:50:54 -0700

    map add

--- src/components/partials/Footer.jsx
+++ src/components/partials/Footer.jsx
@@ -55,7 +55,7 @@ const Footer = () => {
         </div>
 
         {/* Right: Embedded Map */}
-        <div className="footer-right">
+        {/* <div className="footer-right">
           <iframe
             title="Location Map"
             src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3502.7179143871945!2d77.21565591508348!3d28.610143982423364!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x390ce3b58d7b7d2f%3A0xa3d21810e07e2f1e!2sSaket%2C%20New%20Delhi%2C%20Delhi!5e0!3m2!1sen!2sin!4v1687881879734!5m2!1sen!2sin"
@@ -65,7 +65,19 @@ const Footer = () => {
             allowFullScreen=""
             loading="lazy"
           ></iframe>
-        </div>
+        </div> */}
+       <div className="footer-right" style={{ width: '100%', height: '300px' }}>
+    <iframe
+      title="V Hotel The Grand Shoba Location"
+      src="https://maps.google.com/maps?q=Hotel%20The%20Grand%20Shoba%20Mahipalpur%20New%20Delhi&output=embed"
+      width="100%"
+      height="100%"
+      style={{ border: 0 }}
+      allowFullScreen
+      loading="lazy"
+    />
+  </div>
+
       </div>
 
       {/* Bottom Strip */}
